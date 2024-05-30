149879a26f3f18ba4c88f5ba4c5d19becd88d682
Aman Thakur <106446742+aman7thakurr@users.noreply.github.com>
2024-05-30 15:00:22 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,15 @@
+// alert(3+3)//don't use alert in nodejs it will give you error. but you can use it in normal files that are compiled by browsers
+ let string ="this is a string" //string datatype
+ let number = 123 //numbers/integer
+ let boolean = true // true/false
+ let item = null //null is a standalone value  and it's datatype is object
+ let state //undefined
+
+ //symbol => unique
+ 
+ // ***Non primitive data types***
+
+ //object
+ //array
+
+ console.log(typeof item);
