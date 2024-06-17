d560d0d9516b06f38be80becd159bda8e7544bc6
Aman Thakur <106446742+aman7thakurr@users.noreply.github.com>
2024-06-17 06:52:43 +0000

    coversion update

--- 01_basics/03_conversion_operation.js
+++ 01_basics/03_conversion_operation.js
@@ -41,12 +41,12 @@ let negValue = -value
 // console.log(2%3);
 
 let str1 = "hello"
-let str2 = " hitesh"
+let str2 = " Aman"
 
 let str3 = str1 + str2
 // console.log(str3);
 
-// console.log("1" + 2);
+ console.log("1" + 2);
 // console.log(1 + "2");
 // console.log("1" + 2 + 2);
 // console.log(1 + 2 + "2");
