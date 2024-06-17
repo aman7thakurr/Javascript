fdcf7070cd9b7898abd343f1e7dd9d040c970170
Aman Thakur <106446742+aman7thakurr@users.noreply.github.com>
2024-06-17 07:07:55 +0000

    conversion update

--- 01_basics/03_conversion_operation.js
+++ 01_basics/03_conversion_operation.js
@@ -46,12 +46,12 @@ let str2 = " Aman"
 let str3 = str1 + str2
 // console.log(str3);
 
- console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+ //console.log("1" + 2); //output : 12
+// console.log(1 + "2");//output : 12
+// console.log("1" + 2 + 2);//output : 122
+// console.log(1 + 2 + "2");//output : 32
 
-// console.log( (3 + 4) * 5 % 3);
+// console.log( (3 + 4) * 5 % 3);//output : 2
 
 // console.log(+true);
 // console.log(+"");
