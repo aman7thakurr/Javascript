fbdda59b7fe64902f7bfd229049d551d02e439ce
Aman Thakur <106446742+aman7thakurr@users.noreply.github.com>
2024-06-17 06:30:51 +0000

    rules

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,7 +3,16 @@
 Automatically
 Using var
 Using let
-Using const */
+Using const 
+*****-------------------*************
+The general rules for constructing names for variables (unique identifiers) are:
+
+Names can contain letters, digits, underscores, and dollar signs.
+Names must begin with a letter.
+Names can also begin with $ and _ (but we will not use it in this tutorial).
+Names are case sensitive (y and Y are different variables).
+Reserved words (like JavaScript keywords) cannot be used as names.
+*/
 
 const userId = 112 /* const is used to declare variables that are constant and cannot be reassigned. 
 but it does prevent reassignment of the variable.
