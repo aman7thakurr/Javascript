c2adde68cc13bc42a47948ca664fc8137d792e90
Aman Thakur <106446742+aman7thakurr@users.noreply.github.com>
2024-06-18 12:08:08 +0000

    update

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -26,3 +26,19 @@ function testNum() {
   console.log(x);
 }
 testNum();
+
+let s = "00000001111111";
+let l = 0, r = s.length - 1, ans = -1;
+
+while(l <= r) {
+
+  var mid = Math.floor((l + r) / 2);
+   if(s[mid] == '1') {
+       ans = mid;
+       r = mid - 1;
+   }
+   else {
+       l = mid + 1;
+   }
+}
+console.log(ans);
