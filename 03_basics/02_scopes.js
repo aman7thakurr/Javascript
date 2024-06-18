d918d0000c6f5ec0a7fec3ae311bf26d4bb465e1
Aman Thakur <106446742+aman7thakurr@users.noreply.github.com>
2024-06-18 10:20:27 +0000

    update

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,13 +1,14 @@
 // console.log(name);
 var name = "Aman";
 
-(function() {
-  console.log(name);
-  var name = "Aman Thakur";
-})();
+// (function() {
+//   console.log(name);
+//   var name = "Aman Thakur";
+// })();
 
 function sayHelloWorld() {
   return sayGoodbyWorld();
+
   var sayGoodbyWorld = function() {
     return "Hello, World!";
   };
@@ -15,4 +16,13 @@ function sayHelloWorld() {
     return "Goodbye, World!";
   }
 }
-console.log(sayHelloWorld());
+// console.log(sayHelloWorld());
+var x = 10;
+function testNum() {
+  console.log(x);
+  if (true) {
+    var x = 20;
+  }
+  console.log(x);
+}
+testNum();
