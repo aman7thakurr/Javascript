d557bded566695ce13ba570f4ff4f44a63b34e9e
Aman Thakur <106446742+aman7thakurr@users.noreply.github.com>
2024-06-18 05:16:24 +0000

    exec and test

--- 01_basics/10_regex.js
+++ 01_basics/10_regex.js
@@ -1,2 +1,5 @@
-console.log(/how/g.exec("hello ! how are you?"))
+//exec searches provided string and it returns object if no match found it returns null
+console.log(/how/gi.exec("hello ! How are you?"))
+
+//test searches the string for the provided pattern, and returns true or false depending on the result
 console.log(/how/i.test("hello ! How are you?"))
