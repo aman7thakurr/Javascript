ea027203254fae110766320b8f119a798e8c5042
Aman Thakur <106446742+aman7thakurr@users.noreply.github.com>
2024-06-18 04:34:40 +0000

    type conversion

---
+++ 01_basics/09_type_Conversion.js
@@ -0,0 +1,15 @@
+// Converting Strings to Numbers
+console.log(typeof Number("123"));
+
+// Converting Numbers to Strings
+console.log(typeof String(123));
+
+// Converting Dates to Numbers
+const d = new Date()
+console.log(typeof Number(d));
+
+// Converting Numbers to Dates
+
+
+// Converting Booleans to Numbers
+// Converting Numbers to Booleans 
