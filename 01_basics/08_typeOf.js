30138c2e948f3f519649a8fcda14d3a6b0cdeb40
Aman Thakur <106446742+aman7thakurr@users.noreply.github.com>
2024-06-18 04:20:54 +0000

    typeof

---
+++ 01_basics/08_typeOf.js
@@ -0,0 +1,21 @@
+const name = "Aman" //string
+console.log(typeof name); //type: string
+
+
+const num = 12 // number
+console.log(typeof num); //type: number
+
+const bool = true //boolean
+console.log(typeof bool); // type : boolean
+
+const  abc =()=>{ // function
+    return ""
+}
+console.log(typeof abc); // type : function
+
+const obj ={ //object
+    name:"aman thakur",
+
+}
+console.log(typeof obj);// type : object
+
